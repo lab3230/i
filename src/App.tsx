1d5baf831a515d40dc09a48040c255d576dfaf70
takahiro-tanaka-codespaces <116662574+takahiro-tanaka-codespaces@users.noreply.github.com>
2023-06-07 00:35:01 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import './App.css'
 
 function App() {
 
+  const func = {
+    onclick: {
+      gridLayout: () => alert('工事中です!'),
+      handWriting: () => alert('工事中です!'),
+    }
+  }
+
   return (
-    <div className="App">
-      <a href='./repositories/UI-HandWriting' >UI-HandWriting : 手書きコントロール</a>
-    </div>
+    <div className="background">
+      <div className='buttons'>
+        {/* <a href='./repositories/UI-GridLayout'>グリッドレイアウトコントロール</a> */}
+        <a onClickCapture={func.onclick.gridLayout}>グリッドレイアウトコントロール</a>
+        {/* <a href='./repositories/UI-HandWriting' onClickCapture={e => { e.stopPropagation(); func.onclick.gridLayout }}>手書きコントロール</a> */}
+        <a onClickCapture={func.onclick.gridLayout}>手書きコントロール</a>
+      </div>
+    </div >
   )
 }
 
