d68ce062284d89684cf424770af7642949505c7b
takahiro-tanaka-codespaces <116662574+takahiro-tanaka-codespaces@users.noreply.github.com>
2023-06-08 02:40:37 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="background">
       <div className='buttons'>
-        {/* <a href='./repositories/UI-GridLayout'>グリッドレイアウトコントロール</a> */}
-        <a onClickCapture={func.onclick.gridLayout}>グリッドレイアウトコントロール</a>
+        <a href='./repositories/UI-GridLayout'>グリッドレイアウトコントロール</a>
+        {/* <a onClickCapture={func.onclick.gridLayout}>グリッドレイアウトコントロール</a> */}
         <a href='./repositories/UI-HandWriting' onClickCapture={e => { e.stopPropagation(); func.onclick.gridLayout }}>手書きコントロール</a>
         {/* <a onClickCapture={func.onclick.gridLayout}>手書きコントロール</a> */}
       </div>
