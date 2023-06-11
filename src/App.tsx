02e233b3fa8535181c1a82c2568cc9d12c7e7789
takahiro-tanaka-codespaces <116662574+takahiro-tanaka-codespaces@users.noreply.github.com>
2023-06-12 00:37:24 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,16 @@ function App() {
     onclick: {
       gridLayout: () => alert('工事中です!'),
       handWriting: () => alert('工事中です!'),
-    }
+    },
+    onClickCapture: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => { e.stopPropagation(); func.onclick.gridLayout },
   }
 
   return (
     <div className="background">
       <div className='buttons'>
-        {/* <a href='./repositories/UI-GridLayout'>グリッドレイアウトコントロール</a> */}
-        <a onClickCapture={func.onclick.gridLayout}>グリッドレイアウトコントロール</a>
-        <a href='./repositories/UI-HandWriting' onClickCapture={e => { e.stopPropagation(); func.onclick.gridLayout }}>手書きコントロール</a>
+        <a href='./repositories/UI-GridLayout' onClickCapture={func.onClickCapture}>グリッドレイアウトコントロール</a>
+        {/* <a onClickCapture={func.onclick.gridLayout}>グリッドレイアウトコントロール</a> */}
+        <a href='./repositories/UI-HandWriting' onClickCapture={func.onClickCapture}>手書きコントロール</a>
         {/* <a onClickCapture={func.onclick.gridLayout}>手書きコントロール</a> */}
       </div>
     </div >
